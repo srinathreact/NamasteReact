9e00dad857401f43b953d2cfb7f3d08de4d159ac
Srinath <srinathangular10@gmail.com>
2023-12-07 22:44:59 -0800

    Testing-div tag is replace or not

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-// const heading = React.createElement(
-//     'h1',
-//     {id:'heading'},
-//     'hello world from React'
-// );
-// console.log(heading);
+const heading = React.createElement(
+    'h1',
+    {id:"heading",xyz:"abc"},
+    "Hello World From React"
+);
 
-//         const root  = ReactDOM.createRoot(document.getElementById("root"));
-//         root.render(heading);
-
-const parent = React.createElement("div",{id:"parent"},[
-   React.createElement("div",{id:"child"},[
-      React.createElement("h1",{},"I am an h1 tag"),
-      React.createElement("h2",{},"I am an h2 tag")
-   ]),
-   React.createElement("div",{id:"child2"},[
-      React.createElement("h1",{},"I am an h1 tag"),
-      React.createElement("h2",{},"I am an h2 tag")
-   ])
-])
-console.log(parent);
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
